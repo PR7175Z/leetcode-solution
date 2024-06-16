def2904b3c4d0419136fa8517a92cf788f48a838
PR7175Z <aadimpraz@gmail.com>
2024-06-16 23:15:55 +0545

    remove element function code

--- removeelement/main.py
+++ removeelement/main.py
@@ -1,23 +1,13 @@
-# class Solution(object):
-#     def removeElement(self, nums, val):
-#         for x in range(0, len(nums)):
-#             if nums[x] == val:
-#                 nums.pop(x)
+class Solution(object):
+    def removeElement(self, nums, val):
+        nums.remove(val)
+        for x in nums:
+            if x == val:
+                nums.remove(x)
 
-#         k = len(nums)
-#         return k
+        k = len(nums)
+        return k
     
-# c = Solution()
-# print(c.removeElement([1,1,2,1,2,3,4,5,5], 1))
+c = Solution()
+print(c.removeElement([1,1,2,1,2,3,4,5,5], 1))
 
-
-x=0
-nums = [1,1,2,1,2,3,4,5,5]
-val = 1
-running = True
-for x in nums:
-    if x == val:
-        nums.remove(x)
-    print(val)
-    
-print(nums)
